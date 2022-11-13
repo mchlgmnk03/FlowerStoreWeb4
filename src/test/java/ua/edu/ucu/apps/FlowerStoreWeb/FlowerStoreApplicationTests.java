d42f25509877497ba8463bb956373e9a5f53665e
misha <mykhailo.humeniuk@ucu.edu.ua>
2022-11-13 23:29:10 +0200

    failed tests

--- src/test/java/ua/edu/ucu/apps/FlowerStoreWeb/FlowerStoreApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/FlowerStoreWeb/FlowerStoreApplicationTests.java
@@ -3,7 +3,7 @@ package ua.edu.ucu.apps.FlowerStoreWeb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FlowerStoreApplicationTests.class)
 class FlowerStoreApplicationTests {
 
     @Test
